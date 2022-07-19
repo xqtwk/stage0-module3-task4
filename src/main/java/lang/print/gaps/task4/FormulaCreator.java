5e78f9c26881573a3d6eac64cb4b9c271d63a4c5
Richard <xqtwkk@gmail.com>
2022-07-19 14:53:21 +0300

    attempt 2

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -4,6 +4,7 @@ public class FormulaCreator {
     public static void main(String[] args) {
         double a = 2;
         double b = 5;
-        System.out.println((9*Math.pow(a,2)-5*b+2+a-7)*((a+b-4*a*b)/2));
+        double result = (9*Math.pow(a,2)-(5*b)+2+a-7)*((a+b-4*(a*b))/2);
+        System.out.println(result);
     }
 }
