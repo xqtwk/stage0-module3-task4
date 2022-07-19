34c545181b2c8a126f4eac81f9f51f0b8a969e0e
Richard <xqtwkk@gmail.com>
2022-07-19 14:47:09 +0300

    attempt 1

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a = 2;
+        double b = 5;
+        System.out.println((9*Math.pow(a,2)-5*b+2+a-7)*((a+b-4*a*b)/2));
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,6 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        System.out.println((int)'c' + " " + (int)'h' + " " + (int)'a' + " " + (int)'r');
     }
 }
